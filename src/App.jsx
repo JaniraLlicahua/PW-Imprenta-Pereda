5d9ea53601e981c88734f729a8dedacc8f4e0ef1
Case1490 <renzoramos414@gmail.com>
2025-06-07 11:03:45 -0500

    contact finish

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Quotes from "./pages/Quotes";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/servicios" element={<Services />} />
         <Route path="/cotizaciones" element={<Quotes />} />
+        <Route path="/contacto" element={<Contact />} />
         <Route path="/iniciar-sesion" element={<Login />} />
         <Route path="/registrar" element={<Register />} />
       </Routes>
